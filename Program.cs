49d5c171d0f4516c4a21bf52f26ac65eed0c4ebf
Kelison <kls_kelison@hotmail.com>
2024-09-25 01:00:10 -0300

    Desafio dio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Iphone");
+Iphone I1 = new Iphone("15444688", "Iphone 15", "555", 264);
+I1.Ligar();
+I1.InstalarAplicativo("oneDrive");
+
+Console.WriteLine("Nokia");
+Nokia N1 = new Nokia("22235648", "Nokia 7", "666655", 128);
+N1.ReceberLigacao();
+N1.InstalarAplicativo("WhatsApp");
+
+
+
+// TODO: Realizar os testes com as classes Nokia e Iphone;
